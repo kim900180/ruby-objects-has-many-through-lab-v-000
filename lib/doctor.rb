0ed9c77c45f7fc0ea22d6a469db0813a19855f1c
Jae Yong Kim <kim900180@gmail.com>
2018-08-13 16:15:56 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+  end
+
+  def patients
+  end
+
+  def self.all
+    @@all
+  end
+end
